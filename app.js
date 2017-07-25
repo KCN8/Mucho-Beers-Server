5f2a3e4e960940d818ef5738da4288cfdcb004dd
Casey Herold <caseynate@gmail.com>
2017-07-25 14:43:49 -0600

    forgot cors middleware

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 const routes = require('./routes')
+const cors = require('cors')
 
+app.use(cors())
 app.use('/', routes)
 
 app.listen(port)
